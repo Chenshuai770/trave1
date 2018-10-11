eb4b3aa850a9b9e2ff35ec5b4a2855d946c41d65
chenshuai <770456288@qq.ocm>
2018-10-11 18:11:11 +0800

    [add] better-scroll

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -4,8 +4,19 @@
       <div class="title border-topbottom">
         当前城市
       </div>
-
       <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">北京</div>
+        </div>
         <div class="button-wrapper">
           <div class="button">北京</div>
         </div>
@@ -15,11 +26,90 @@
       <div class="title border-topbottom">
         热门城市
       </div>
+      <div class="button-list">
+        <div class="button-wrapper">
+          <div class="button">杭州</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">上海</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">深圳</div>
+        </div>
+        <div class="button-wrapper">
+          <div class="button">宁波</div>
+        </div>
+
+      </div>
     </div>
     <div class="area">
       <div class="title border-topbottom">
         A
       </div>
+      <div class="item-list">
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+
+      </div>
+
+      <div class="title border-topbottom">
+        A
+      </div>
+      <div class="item-list">
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+
+      </div>
+
+
+      <div class="title border-topbottom">
+        A
+      </div>
+      <div class="item-list">
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+        <div class="item border-bottom">
+          阿拉尔
+        </div>
+
+      </div>
+
     </div>
 
   </div>
@@ -32,7 +122,16 @@
 </script>
 
 <style lang="stylus" scoped>
-  .title{
+  .list{
+    overflow: hidden;
+    position: absolute;
+    top: 1.58rem;
+    left: 0;
+    right: 0;
+    bottom: 0;
+  }
+
+  .title {
     line-height: .44rem;
     background: #eee;
     padding-left: .2rem;
@@ -40,25 +139,36 @@
     font-size: .26rem;
   }
 
-  .border-topbottom{
-
-    border-bottom-width: 1rem;
-    border-bottom-color: red;
+  .border-topbottom {
+    border-bottom: 0.01rem solid #ccc;
   }
 
-  .button-list{
+  .button-list {
     overflow: hidden;
-    padding: .1rem;
+    padding: .1rem .6rem .1rem .1rem;
   }
-  .button-wrapper{
+
+  .button-wrapper {
     float: left;
     width: 33.33%;
   }
-  .button{
+
+  .button {
     margin: .1rem;
     text-align: center;
-    border: 0.2rem ;
-    solid:#ccc
+    border: 0.02rem solid #ccc;
+    padding: 0.1rem 0rem;
+    border-radius: .03rem;
+  }
+
+  .item {
+    line-height: .76rem;
+    color: #666;
+    padding-left: 0.2rem;
+  }
+
+  .border-bottom {
+    border-bottom: 0.01rem solid #ccc;
   }
 
 </style>
